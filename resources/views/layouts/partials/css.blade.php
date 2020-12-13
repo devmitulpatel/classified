57c250ebe36a96a28b42e4c62279dbe2d35f4387
msllp <48418814+msllp@users.noreply.github.com>
2020-12-13 23:31:25 +0530

    changes

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -1,7 +1,18 @@
 <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('vendors/font-awesome/css/fontawesome.css')}}">
-<link rel="stylesheet" href="{{asset('vendors/magnific-popup/magnific-popup.css')}}">
-<link rel="stylesheet" href="{{asset('vendors/slick/slick.css')}}">
-<link rel="stylesheet" href="{{asset('vendors/animate.css')}}">
 
-<link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+@php
+$css=[
+    'vendors/font-awesome/css/fontawesome.css',
+    'vendors/magnific-popup/magnific-popup.css',
+    'vendors/slick/slick.css',
+    'vendors/animate.css',
+    'css/vendor.css'
+
+];
+
+@endphp
+
+@foreach($css as $url)
+    <link rel="stylesheet" href="{{asset($url)}}">
+@endforeach
