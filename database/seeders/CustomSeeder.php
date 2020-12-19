0147f777475f850665d6d6c65a4cb24f6caf8988
mitulclassy <68455017+mitulclassy@users.noreply.github.com>
2020-12-19 16:21:55 +0530

    update

---
+++ database/seeders/CustomSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CustomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $this->call([
+            ExtraUsersToTableSeeder::class,
+            ExtraRolesToUserTable::class,
+            PermisionForExtraRolesSeeder::class,
+            ExtraUsersRolesToTableSeeder::class,
+            DemoCategorySeeder::class,
+            DemoSubCategorySeeder::class,
+            WebsiteSettingTableSeeder::class,
+            EmailSettingTableSeeder::class,
+            PaymentGatewayTableSeeder::class,
+
+
+        ]);
+
+    }
+}
